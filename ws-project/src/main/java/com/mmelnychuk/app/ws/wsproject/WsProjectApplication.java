6f557ebb05aa07a591a3d47eaec0d2584ceddc0d
marynamelnichuk <marynamelnichuk859@gmail.com>
2020-10-04 16:47:57 +0300

    Adding a new spring boot project

---
+++ ws-project/src/main/java/com/mmelnychuk/app/ws/wsproject/WsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mmelnychuk.app.ws.wsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsProjectApplication.class, args);
+	}
+
+}
---
+++ ws-project/src/test/java/com/mmelnychuk/app/ws/wsproject/WsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmelnychuk.app.ws.wsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
